cb1b0bacc04843831047d8c7265d3357f5def6b3
Arya Yudha <aryayudhakp@gmail.com>
2023-12-13 15:00:50 +0700

    new file:   .vscode/settings.json 	new file:   connection.php 	new file:   images/6577fef02a649.png 	new file:   index.php 	new file:   readResep.php 	new file:   resep.php 	new file:   styles/style.css 	new file:   upload.php

---
+++ resep.php
@@ -0,0 +1,26 @@
+<?php require_once 'connection.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Resep Makanan</title>
+</head>
+
+<body>
+  <div>
+    <?php
+    $result = $db->query("SELECT * FROM RESEP");
+    while ($row = $result->fetch_object()) {
+      // Tambahkan hyperlink dengan URL dinamis yang membawa ID resep ke readresep.php
+      echo "<a href='readresep.php?id=$row->Id_resep'>";
+      echo "<img src='images/$row->image' width='200'>";
+      echo "<h1>$row->Judul</h1>";
+      echo "</a>";
+    }
+    ?>
+  </div>
+</body>
+
+</html>
---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'connection.php';
+if (isset($_POST['submit'])) {
+  $judul = $_POST['judulResep'];
+  $bahan = $_POST['bahanResep'];
+  $langkah = $_POST['langkahResep'];
+  if ($_FILES['image']['error'] === 4) {
+    echo "<script> alert('Image tak ditemukan') </script>";
+  } else {
+    $filename = $_FILES['image']['name'];
+    $fileSize = $_FILES['image']['size'];
+    $tmpName = $_FILES['image']['tmp_name'];
+
+    $imageExtension = explode(".", $filename);
+    $imageExtension = strtolower(end($imageExtension));
+    if ($fileSize > 1000000000) {
+      echo "<script> alert('Ukuran Image terlalu besar') </script>";
+    } else {
+      $uniqImageName = uniqid();
+      $uniqImageName = $uniqImageName . '.' . $imageExtension;
+
+      move_uploaded_file($tmpName, 'images/' . $uniqImageName);
+      $query = "INSERT INTO resep VALUES ('', '$judul', '$bahan', '$langkah', CURRENT_TIMESTAMP(), '01', '$uniqImageName')";
+      mysqli_query($db, $query);
+      echo "<script> alert('Berhasil Menambahkan');
+      document.location.href = 'index.php';
+      </script>";
+    }
+  }
+}
